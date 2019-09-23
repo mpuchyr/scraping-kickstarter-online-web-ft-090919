ecb8e42fd302c79208070ce3d4fcc546ed3857d2
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-23 22:32:36 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,13 +2,6 @@
 require 'nokogiri'
 require 'pry'
 
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("dv.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# location: project.css("ul.project-meta span.location-name").text
-# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
@@ -18,43 +11,10 @@ def create_project_hash
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
-      :image_link => project.css("dv.project-thumbnail a img").attribute("src").value,
+      :image_link => project.css("div.project-thumbnail a img").value,
       :description => project.css("p.bbcard_blurb").text,
       :location => project.css("ul.project-meta span.location-name").text,
-      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+      :percent_funded => project.css("ul.project-stats")
     }
   end
-  
-  projects
 end
-
-# require 'nokogiri'
-# require 'pry'
- 
-# # projects: kickstarter.css("li.project.grid_4")
-# # title: project.css("h2.bbcard_name strong a").text
-# # image link: project.css("div.project-thumbnail a img").attribute("src").value
-# # description: project.css("p.bbcard_blurb").text
-# # location: project.css("ul.project-meta span.location-name").text
-# # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
- 
-# def create_project_hash
-#   html = File.read('fixtures/kickstarter.html')
-#   kickstarter = Nokogiri::HTML(html)
- 
-#   projects = {}
- 
-#   kickstarter.css("li.project.grid_4").each do |project|
-#     title = project.css("h2.bbcard_name strong a").text
-#     projects[title.to_sym] = {
-#       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-#       :description => project.css("p.bbcard_blurb").text,
-#       :location => project.css("ul.project-meta span.location-name").text,
-#       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-#     }
-#   end
- 
-#   # return the projects hash
-#   projects
-# end
-
